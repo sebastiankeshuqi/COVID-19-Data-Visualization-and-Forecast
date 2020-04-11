6bc13697b215c4a1517b4dbf7657d0bafa793fa0
sebastiankeshuqi <sebastiankeshuqi@outlook.com>
2020-04-11 11:20:37 +0800

    Training OK

--- Forecast/training.py
+++ Forecast/training.py
@@ -1,197 +1,362 @@
-'''
-Reference: https://colab.research.google.com/drive/1nQYJq1f7f4R0yeZOzQ9rBKgk00AfLoS0#scrollTo=3_15jwwrASTP&forceEdit=true&sandboxMode=true
-           https://www.curiousily.com/posts/time-series-forecasting-with-lstm-for-daily-coronavirus-cases/
-'''
 import torch
-import numpy
-import pandas
-import seaborn
+
+import os
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from pylab import rcParams
 import matplotlib.pyplot as plt
+from matplotlib import rc
 from sklearn.preprocessing import MinMaxScaler
+from pandas.plotting import register_matplotlib_converters
+from torch import nn, optim
 
-# Build the forecast model
+# %matplotlib inline
+# %config InlineBackend.figure_format='retina'
 
+sns.set(style='whitegrid', palette='muted', font_scale=1.2)
 
-class COVIDPredictor(torch.nn.Module):
+HAPPY_COLORS_PALETTE = ["#01BEFE", "#FFDD00", "#FF7D00", "#FF006D", "#93D30C", "#8F00FF"]
 
-    def __init__(self, input_num, hidden_layer_num, recurrent_layer_num, batch_size):
-        '''
-        :paras:recurrent_layer_num – Number of recurrent layers
-        '''
-        super(COVIDPredictor, self).__init__()
-        self.batch_size = batch_size
-        self.input_num = input_num
-        self.hidden_layer_num = hidden_layer_num
-        self.recurrent_layer_num = recurrent_layer_num
-        self.lstm = torch.nn.LSTM(
-            input_size=self.input_num,
-            hidden_size=self.hidden_layer_num,
-            num_layers=self.recurrent_layer_num,
-            dropout=0.6
-        )
-        self.linear = torch.nn.Linear(
-            in_features=hidden_layer_num, out_features=1)
+sns.set_palette(sns.color_palette(HAPPY_COLORS_PALETTE))
 
-    def reset_hidden_state(self):  # Use a stateless LSTM
-        self.hidden = (
-            torch.zeros(self.input_num, self.batch_size,
-                        self.hidden_layer_num),
-            torch.zeros(self.input_num, self.batch_size, self.hidden_layer_num)
-        )
-    def forward(self, inputs):  # Pass the data through the LSTM
-        lstm_out, self.hidden = self.lstm(
-            inputs.view(len(inputs), self.batch_size, -1),
-            self.hidden
-        )
-        last_time_step = lstm_out.view(
-            self.batch_size, len(inputs), self.hidden_layer_num)[-1]
-        y_predict = self.linear(last_time_step)
-        return y_predict
+rcParams['figure.figsize'] = 14, 10
+register_matplotlib_converters()
 
+RANDOM_SEED = 42
+np.random.seed(RANDOM_SEED)
+torch.manual_seed(RANDOM_SEED)
 
-def preprocessing():
-    data_file = pandas.read_csv('time_series_covid19_confirmed_global.csv')
-    data_file = data_file.iloc[:, 4:]
-    daily_cases = data_file.sum()
+df = pd.read_csv('time_series_covid19_confirmed_global.csv')
 
-    days = daily_cases.shape[0]
-    test_size = int(days * 0.2)  # Use 20% data for tests
+df = df.iloc[:, 4:]
 
-    train_data = daily_cases[:-test_size]
-    test_data = daily_cases[-test_size:]
+df.isnull().sum().sum()
 
-    '''
-    Use MinMaxScaler to speed up training and get better performance
-    '''
+"""Everything seems to be in place. Let's sum all rows, so we get the cumulative daily cases:"""
 
-    scaler = MinMaxScaler()
-    scaler = scaler.fit(numpy.expand_dims(train_data, axis=1))
-    train_data = scaler.transform(numpy.expand_dims(train_data, axis=1))
-    test_data = scaler.transform(numpy.expand_dims(test_data, axis=1))
+daily_cases = df.sum(axis=0)
+daily_cases.index = pd.to_datetime(daily_cases.index)
 
-    batch_size = 7
-    X_train, y_train = create_sequences(train_data, batch_size)
-    X_test, y_test = create_sequences(test_data, batch_size)
+plt.plot(daily_cases)
+plt.title("Cumulative daily cases");
 
-    X_train = torch.from_numpy(X_train).float()
-    y_train = torch.from_numpy(y_train).float()
+"""We'll undo the accumulation by subtracting the current value from the previous. We'll preserve the first value of the sequence:"""
 
-    X_test = torch.from_numpy(X_test).float()
-    y_test = torch.from_numpy(y_test).float()
+daily_cases = daily_cases.diff().fillna(daily_cases[0]).astype(np.int64)
 
-    model = COVIDPredictor(
-        input_num=1,
-        hidden_layer_num=512,
-        recurrent_layer_num=1,
-        batch_size=batch_size
-    )
-    model, train_hist, test_hist = train(
-        model,
-        X_train,
-        y_train,
-        X_test,
-        y_test
-    )
+plt.plot(daily_cases)
+plt.title("Daily cases")
 
-    plt.plot(train_hist, label="Training loss")
-    plt.plot(test_hist, label="Test loss")
-    plt.ylim((0, 5))
-    plt.show()
+print(daily_cases.shape)
 
+test_data_size = 20
 
-def train(model, X_train, y_train, test_data=None, y_test=None):
-    loss_fn = torch.nn.MSELoss(reduction='sum')
-    optimiser = torch.optim.Adam(model.parameters(), lr=1e-3)
-    epoch_num = 60
+train_data = daily_cases[:-test_data_size]
+test_data = daily_cases[-test_data_size:]
 
-    train_hist = numpy.zeros(epoch_num)
-    test_hist = numpy.zeros(epoch_num)
+"""We have to scale the data (values will be between 0 and 1) if we want to increase the training speed and performance of the model. We'll use the `MinMaxScaler` from scikit-learn:"""
 
-    for i in range(epoch_num):
-        model.reset_hidden_state()
+scaler = MinMaxScaler()
 
-        y_pred = model(X_train)
+scaler = scaler.fit(np.expand_dims(train_data, axis=1))
 
-        loss = loss_fn(y_pred.float(), y_train)
+train_data = scaler.transform(np.expand_dims(train_data, axis=1))
 
-        if test_data is not None:
-            with torch.no_grad():
-                y_test_pred = model(test_data)
-                test_loss = loss_fn(y_test_pred.float(), y_test)
-            test_hist[i] = test_loss.item()
+test_data = scaler.transform(np.expand_dims(test_data, axis=1))
 
-            if i % 10 == 0:
-                print(
-                    f'Epoch {i} train loss: {loss.item()} test loss: {test_loss.item()}')
-        elif i % 10 == 0:
-            print(f'Epoch {i} train loss: {loss.item()}')
-
-        train_hist[i] = loss.item()
-
-        optimiser.zero_grad()
-
-        loss.backward()
-
-        optimiser.step()
-
-    return model.eval(), train_hist, test_hist
-
-
-def main():
-    data_file = pandas.read_csv('time_series_covid19_confirmed_global.csv')
-    data_file = data_file.iloc[:, 4:]
-    daily_cases = data_file.sum()
-
-    scaler = MinMaxScaler()
-    scaler = scaler.fit(numpy.expand_dims(daily_cases, axis=1))
-    all_data = scaler.transform(numpy.expand_dims(daily_cases, axis=1))
-
-    batch_size = 7
-
-    X_all, y_all = create_sequences(all_data, batch_size)
-    X_all = torch.from_numpy(X_all).float()
-    y_all = torch.from_numpy(y_all).float()
-    model = COVIDPredictor(
-        input_num=1,
-        hidden_layer_num=512,
-        recurrent_layer_num = 1,
-        batch_size=batch_size
-    )
-    model, train_hist, _ = train(model, X_all, y_all)
-    predict_days = 12
-    with torch.no_grad():
-        test_seq = X_all[:1]
-        preds = []
-
-        for _ in range(predict_days):
-            y_test_pred = model(test_seq)
-            pred = torch.flatten(y_test_pred).item()
-            preds.append(pred)
-            new_seq = test_seq.numpy().flatten()
-            new_seq = numpy.append(new_seq, [pred])
-            new_seq = new_seq[1:]
-            test_seq = torch.as_tensor(new_seq).view(1, batch_size, 1).float()
-
-        predicted_cases = scaler.inverse_transform(
-            numpy.expand_dims(preds, axis=0)
-        ).flatten()
-
-        plt.plot(daily_cases, label='Historical Daily Cases')
-        plt.plot(predicted_cases, label='Predicted Daily Cases')
-        plt.show()
-
+"""Currently, we have a big sequence of daily cases. We'll convert it into smaller ones:"""
 
 def create_sequences(data, seq_length):
     xs = []
     ys = []
+
     for i in range(len(data)-seq_length-1):
         x = data[i:(i+seq_length)]
         y = data[i+seq_length]
         xs.append(x)
         ys.append(y)
-    return numpy.array(xs), numpy.array(ys)
+
+    return np.array(xs), np.array(ys)
+
+seq_length = 14
+X_train, y_train = create_sequences(train_data, seq_length)
+X_test, y_test = create_sequences(test_data, seq_length)
+
+X_train = torch.from_numpy(X_train).float()
+y_train = torch.from_numpy(y_train).float()
+
+X_test = torch.from_numpy(X_test).float()
+y_test = torch.from_numpy(y_test).float()
 
 
-if __name__ == '__main__':
-    # preprocessing()
-    main()
+class CoronaVirusPredictor(nn.Module):
+
+  def __init__(self, n_features, n_hidden, seq_len, n_layers=2):
+    super(CoronaVirusPredictor, self).__init__()
+
+    self.n_hidden = n_hidden
+    self.seq_len = seq_len
+    self.n_layers = n_layers
+
+    self.lstm = nn.LSTM(
+      input_size=n_features,
+      hidden_size=n_hidden,
+      num_layers=n_layers,
+      dropout=0.25
+    )
+
+    self.linear = nn.Linear(in_features=n_hidden, out_features=1)
+
+  def reset_hidden_state(self):
+    self.hidden = (
+        torch.zeros(self.n_layers, self.seq_len, self.n_hidden),
+        torch.zeros(self.n_layers, self.seq_len, self.n_hidden)
+    )
+
+  def forward(self, sequences):
+    lstm_out, self.hidden = self.lstm(
+      sequences.view(len(sequences), self.seq_len, -1),
+      self.hidden
+    )
+    last_time_step = \
+      lstm_out.view(self.seq_len, len(sequences), self.n_hidden)[-1]
+    y_pred = self.linear(last_time_step)
+    return y_pred
+
+"""Our `CoronaVirusPredictor` contains 3 methods:
+- constructor - initialize all helper data and create the layers
+- `reset_hidden_state` - we'll use a stateless LSTM, so we need to reset the state after each example
+- `forward` - get the sequences, pass all of them through the LSTM layer, at once. We take the output of the last time step and pass it through our linear layer to get the prediction.
+
+## Training
+
+Let's build a helper function for the training of our model (we'll reuse it later):
+"""
+
+def train_model(
+  model, 
+  train_data, 
+  train_labels, 
+  test_data=None, 
+  test_labels=None
+):
+  loss_fn = torch.nn.MSELoss(reduction='sum')
+
+  optimiser = torch.optim.Adam(model.parameters(), lr=1e-3)
+  num_epochs = 60
+
+  train_hist = np.zeros(num_epochs)
+  test_hist = np.zeros(num_epochs)
+
+  for t in range(num_epochs):
+    model.reset_hidden_state()
+
+    y_pred = model(X_train)
+
+    loss = loss_fn(y_pred.float(), y_train)
+
+    if test_data is not None:
+      with torch.no_grad():
+        y_test_pred = model(X_test)
+        test_loss = loss_fn(y_test_pred.float(), y_test)
+      test_hist[t] = test_loss.item()
+
+      if t % 10 == 0:  
+        print(f'Epoch {t} train loss: {loss.item()} test loss: {test_loss.item()}')
+    elif t % 10 == 0:
+      print(f'Epoch {t} train loss: {loss.item()}')
+
+    train_hist[t] = loss.item()
+    
+    optimiser.zero_grad()
+
+    loss.backward()
+
+    optimiser.step()
+  
+  return model.eval(), train_hist, test_hist
+
+"""Note that the hidden state is reset at the start of each epoch. We don't use batches of data our model sees every example at once. We'll use mean squared error to measure our training and test error. We'll record both. 
+
+Let's create an instance of our model and train it:
+"""
+
+model = CoronaVirusPredictor(
+  n_features=1, 
+  n_hidden=512, 
+  seq_len=seq_length, 
+  n_layers=2
+)
+model, train_hist, test_hist = train_model(
+  model, 
+  X_train, 
+  y_train, 
+  X_test, 
+  y_test
+)
+
+"""Let's have a look at the train and test loss:"""
+
+plt.plot(train_hist, label="Training loss")
+plt.plot(test_hist, label="Test loss")
+plt.ylim((0, 5))
+plt.legend();
+
+"""Our model's performance doesn't improve after 15 epochs or so. Recall that we have very little data. Maybe we shouldn't trust our model that much?
+
+## Predicting daily cases
+
+Our model can (due to the way we've trained it) predict only a single day in the future. We'll employ a simple strategy to overcome this limitation. Use predicted values as input for predicting the next days:
+"""
+
+with torch.no_grad():
+  test_seq = X_test[:1]
+  preds = []
+  for _ in range(len(X_test)):
+    y_test_pred = model(test_seq)
+    pred = torch.flatten(y_test_pred).item()
+    preds.append(pred)
+    new_seq = test_seq.numpy().flatten()
+    new_seq = np.append(new_seq, [pred])
+    new_seq = new_seq[1:]
+    test_seq = torch.as_tensor(new_seq).view(1, seq_length, 1).float()
+
+"""We have to reverse the scaling of the test data and the model predictions:"""
+
+true_cases = scaler.inverse_transform(
+    np.expand_dims(y_test.flatten().numpy(), axis=0)
+).flatten()
+
+predicted_cases = scaler.inverse_transform(
+  np.expand_dims(preds, axis=0)
+).flatten()
+
+"""Let's look at the results:"""
+
+plt.plot(
+  daily_cases.index[:len(train_data)], 
+  scaler.inverse_transform(train_data).flatten(),
+  label='Historical Daily Cases'
+)
+
+plt.plot(
+  daily_cases.index[len(train_data):len(train_data) + len(true_cases)], 
+  true_cases,
+  label='Real Daily Cases'
+)
+
+plt.plot(
+  daily_cases.index[len(train_data):len(train_data) + len(true_cases)], 
+  predicted_cases, 
+  label='Predicted Daily Cases'
+)
+
+plt.legend();
+
+"""As expected, our model doesn't perform very well. That said, the predictions seem to be in the right ballpark (probably due to using the last data point as a strong predictor for the next).
+
+## Use all data for training
+
+Now, we'll use all available data to train the same model:
+"""
+
+scaler = MinMaxScaler()
+
+scaler = scaler.fit(np.expand_dims(daily_cases, axis=1))
+
+all_data = scaler.transform(np.expand_dims(daily_cases, axis=1))
+
+all_data.shape
+
+"""The preprocessing and training steps are the same:"""
+
+X_all, y_all = create_sequences(all_data, seq_length)
+
+X_all = torch.from_numpy(X_all).float()
+y_all = torch.from_numpy(y_all).float()
+
+model = CoronaVirusPredictor(
+  n_features=1, 
+  n_hidden=512, 
+  seq_len=seq_length, 
+  n_layers=2
+)
+model, train_hist, _ = train_model(model, X_all, y_all)
+
+"""## Predicting future cases
+
+We'll use our "fully trained" model to predict the confirmed cases for 12 days into the future:
+"""
+
+DAYS_TO_PREDICT = 12
+
+with torch.no_grad():
+  test_seq = X_all[:1]
+  preds = []
+  for _ in range(DAYS_TO_PREDICT):
+    y_test_pred = model(test_seq)
+    pred = torch.flatten(y_test_pred).item()
+    preds.append(pred)
+    new_seq = test_seq.numpy().flatten()
+    new_seq = np.append(new_seq, [pred])
+    new_seq = new_seq[1:]
+    test_seq = torch.as_tensor(new_seq).view(1, seq_length, 1).float()
+
+"""As before, we'll inverse the scaler transformation:"""
+
+predicted_cases = scaler.inverse_transform(
+  np.expand_dims(preds, axis=0)
+).flatten()
+
+"""To create a cool chart with the historical and predicted cases,
+we need to extend the date index of our data frame:"""
+
+daily_cases.index[-1]
+
+predicted_index = pd.date_range(
+  start=daily_cases.index[-1],
+  periods=DAYS_TO_PREDICT + 1,
+  closed='right'
+)
+
+predicted_cases = pd.Series(
+  data=predicted_cases,
+  index=predicted_index
+)
+
+plt.plot(predicted_cases, label='Predicted Daily Cases')
+plt.show();
+
+"""Now we can use all the data to plot the results:"""
+
+plt.plot(daily_cases, label='Historical Daily Cases')
+plt.plot(predicted_cases, label='Predicted Daily Cases')
+plt.show();
+
+"""Our model thinks that things will level off. Note that the more you go into the future, the more you shouldn't trust your model predictions.
+
+## Conclusion
+
+Well done! You learned how to use PyTorch to create a Recurrent Neural Network that works with Time Series data. The model performance is not that great, but this is expected, given the small amounts of data.
+
+- [Run the complete notebook in your browser (Google Colab)](https://colab.research.google.com/drive/1nQYJq1f7f4R0yeZOzQ9rBKgk00AfLoS0)
+- [Read the Getting Things Done with Pytorch book](https://github.com/curiousily/Getting-Things-Done-with-Pytorch)
+
+The problem of predicting daily Covid-19 cases is a hard one. We're amidst an outbreak, and there's more to be done. Hopefully, everything will be back to normal after some time.
+
+## References
+
+- [Sequence Models PyTorch Tutorial](https://pytorch.org/tutorials/beginner/nlp/sequence_models_tutorial.html)
+- [LSTM for time series prediction](https://towardsdatascience.com/lstm-for-time-series-prediction-de8aeb26f2ca)
+- [Time Series Prediction using LSTM with PyTorch in Python](https://stackabuse.com/time-series-prediction-using-lstm-with-pytorch-in-python/)
+- [Stateful LSTM in Keras](https://philipperemy.github.io/keras-stateful-lstm/)
+- [LSTMs for Time Series in PyTorch](https://www.jessicayung.com/lstms-for-time-series-in-pytorch/)
+- [Novel Coronavirus (COVID-19) Cases, provided by JHU CSSE](https://github.com/CSSEGISandData/COVID-19)
+- [covid-19-analysis](https://github.com/AaronWard/covid-19-analysis)
+- [How does Coronavirus compare to Ebola, SARS, etc?](https://www.youtube.com/watch?v=6dDD2tHWWnU)
+- [Worldometer COVID-19 Coronavirus Outbreak](https://www.worldometers.info/coronavirus/)
+- [How contagious is the Wuhan Coronavirus? (Ro)](https://www.worldometers.info/coronavirus/#repro)
+- [Systemic Risk of Pandemic via Novel Pathogens - Coronavirus: A Note](https://www.academia.edu/41743064/Systemic_Risk_of_Pandemic_via_Novel_Pathogens_-_Coronavirus_A_Note)
+- [Statistical Consequences of Fat Tails: Real World Preasymptotics, Epistemology, and Applications](https://www.researchers.one/article/2020-01-21)
+"""
